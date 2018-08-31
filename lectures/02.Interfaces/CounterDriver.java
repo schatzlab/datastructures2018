8ed9f4351244d68cda7cdb1e72ebed0115962cca
Mike Schatz <mschatz@cs.jhu.edu>
2018-08-31 09:33:35 -0400

    more cleanup

--- lectures/02.Interfaces/CounterDriver.java
+++ lectures/02.Interfaces/CounterDriver.java
@@ -1,6 +1,7 @@
 public class CounterDriver {
     public static void main(String[] args) {
         Counter c = null;
+
         if ((args.length > 0) && (args[0].equals("Weird"))) {
             c = new WeirdCounter();
         } else {
