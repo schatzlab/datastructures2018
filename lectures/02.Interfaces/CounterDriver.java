d417b115395b496edf7aa68c5481c39ab6b69e29
Mike Schatz <mschatz@cs.jhu.edu>
2018-08-30 23:12:17 -0400

    Rename to CounterDriver

---
+++ lectures/02.Interfaces/CounterDriver.java
@@ -0,0 +1,24 @@
+public class CounterTest {
+    public static void main(String[] args)
+    {
+        Counter c = null;
+        if ((args.length > 0) && (args[0].equals("Weird"))) {
+          c = new WeirdCounter();
+        }
+        else { 
+          c = new MyCounter();
+        }
+
+        System.out.println("Counter is " + c.value());
+
+        c.up();
+
+        System.out.println("Counter is " + c.value());
+
+        c.down();
+
+        System.out.println("Counter is " + c.value());
+    }
+}
+
+
