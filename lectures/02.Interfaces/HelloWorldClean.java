596af576d5187f23cc0e78e6cb056d5f074392cd
Mike Schatz <mschatz@cs.jhu.edu>
2018-08-30 22:15:50 -0400

    add checkstyle clean version

---
+++ lectures/02.Interfaces/HelloWorldClean.java
@@ -0,0 +1,16 @@
+/** HelloWorld class. */
+public final class HelloWorldClean {
+
+    /** private constructor */
+    private HelloWorldClean() {
+    }
+
+    /**
+     * Main method.
+     * @param argv Command Line Arguments
+     */
+    public static void main(String[] argv) {
+        System.out.println("Hello World!");
+    }
+}
+
