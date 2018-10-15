5637568c6129d8feb4c120665d18c981b331f2d2
Mike Schatz <mschatz@cs.jhu.edu>
2018-10-14 21:37:51 -0400

    add

--- lectures/20.Sets/runTestUniqueList.sh
+++ lectures/20.Sets/runTestUniqueList.sh
@@ -5,16 +5,16 @@ if [ ! -r random10k.txt ];  then seq 1 10000   | awk '{print int(rand()*100000)}
 if [ ! -r random100k.txt ]; then seq 1 100000  | awk '{print int(rand()*100000)}' > random100k.txt; fi
 
 echo "Testing random1k"
-time java Unique < random1k.txt > bla
-wc -l bla
+time java UniqueList < random1k.txt > bla2
+wc -l bla2
 
 echo "Testing random10k"
-time java Unique < random10k.txt > bla
-wc -l bla
+time java UniqueList < random10k.txt > bla2
+wc -l bla2
 
 echo "Testing random100k"
-time java Unique < random100k.txt > bla
-wc -l bla
+time java UniqueList < random100k.txt > bla2
+wc -l bla2
 
 
 
@@ -25,13 +25,13 @@ if [ ! -r spread10k.txt ];  then seq 1 100000  | awk '{print int(rand()*10000)}'
 if [ ! -r spread100k.txt ]; then seq 1 100000  | awk '{print int(rand()*100000)}' > spread100k.txt; fi
 
 echo "Testing spread1k"
-time java Unique < spread1k.txt > bla
-wc -l bla
+time java UniqueList < spread1k.txt > bla2
+wc -l bla2
 
 echo "Testing spread10k"
-time java Unique < spread10k.txt > bla
-wc -l bla
+time java UniqueList < spread10k.txt > bla2
+wc -l bla2
 
 echo "Testing spread100k"
-time java Unique < spread100k.txt > bla
-wc -l bla
+time java UniqueList < spread100k.txt > bla2
+wc -l bla2
